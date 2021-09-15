56f13ab94012c71ae6df47d94ce5d27830e7ee3e
shivanand patil <shivanandp@orowealth.com>
2021-09-15 18:27:33 +0530

    Add code for API call and the video element
    Signed-off-by: shivanand patil <shivanandp@orowealth.com>

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 console.log(API_URL);
 
 function renderDynamicContent() {
-    document.getElementById('dynamic-content').innerHTML = "Dynamic content"
+    document.getElementById('dynamic-content').innerHTML = "Dynamic content";
+
+    const makeServerCall = function (data, cb) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", API_URL, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+        xhr.onload = function() {
+            console.log("response")
+            console.log(this.responseText);
+            var response = JSON.parse(this.responseText);
+            return cb(response);
+        }
+    };
+
+    makeServerCall({}, function (response) {
+
+        for (respose )
+        document.getElementById('dynamic-content').innerHTML = 
+    })
+
+    
 }
 
